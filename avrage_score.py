618d2d833198ea738477b7ddb177176c33b2a95e
OUALID AAZAB <160775667+Oualidaazab@users.noreply.github.com>
2025-06-06 16:28:48 +0200

    Update avrage_score.py

--- avrage_score.py
+++ avrage_score.py
@@ -1,3 +1,27 @@
+#***************************************************************************************************
+#"""
+#The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+#Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+#Example
+# 'alpha' : [20,30,40]
+# 'beta' : [30,50,70]
+# 'beta' : [30,50,70]
+# query_name= 'beta' 
+
+#The query_name is 'beta'. beta's average score is
+# (30+50+70)/30 = 50.0 
+
+#Input Format
+#The first line contains the integer
+#, the number of students' records. The next
+
+#lines contain the names and marks obtained by a student, each value separated by a space. The final line contains query_name, the name of a student to query.
+
+#Constraints
+# 2<n<10
+# 0<marks [i]<100
+# length of marks arrays = 3 
+
 if __name__ == '__main__':
     n = int(input("Enter how many student in this class :"))
     student_marks = {}
