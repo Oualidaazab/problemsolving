9f1c9f88de68788cba745f7aaf3172e14b620018
OUALID AAZAB <160775667+Oualidaazab@users.noreply.github.com>
2025-06-08 20:14:40 +0200

    Update Hash.py

---
+++ Hash.py
@@ -0,0 +1,11 @@
+# ****************************************************************| 
+# https://www.hackerrank.com/challenges/python-tuples/problem     |
+#  FROM HACKER RANK                                               |
+#*****************************************************************| 
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    t=tuple(integer_list) 
+    print(hash(t)) 
