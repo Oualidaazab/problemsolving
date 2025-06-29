4217422afce3c8a5f0d5656df2808f11f9f85d99
oualid <aazabwalid20@gmail.com>
2025-06-29 19:12:56 +0100

    FORM using python

---
+++ WELCOME.py
@@ -0,0 +1,15 @@
+# Input format: two space-separated integers N and M
+N, M = map(int, input().split())
+
+# Top half of the mat
+for i in range(N // 2):
+    pattern = ".|." * (2 * i + 1)
+    print(pattern.center(M, "-"))
+
+# Middle line
+print("WELCOME".center(M, "-"))
+
+# Bottom half (mirror of the top)
+for i in range(N // 2 - 1, -1, -1):
+    pattern = ".|." * (2 * i + 1)
+    print(pattern.center(M, "-"))
