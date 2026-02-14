fab7f0fd58971a801dad0624c2020656d887761a
OUALID AAZAB <160775667+Oualidaazab@users.noreply.github.com>
2026-02-14 08:56:31 +0100

    Restore uppercase check and add comment
    Reinstate check for uppercase characters and add comment.

--- Validation_method.py
+++ Validation_method.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
     print(any(c.isalpha() for c in s))     
     print(any(c.isdigit() for c in s))    
     print(any(c.islower() for c in s))    
-    print(any(c.isupper() for c in s))  
+    print(any(c.isupper() for c in s))   
+# this a challange from hackerRank 
